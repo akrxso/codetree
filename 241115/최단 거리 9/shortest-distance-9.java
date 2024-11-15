0f15c49c884214eb0fe203f0c699645b1123e99d
akrxso <143086344+akrxso@users.noreply.github.com>
2024-11-15 18:36:40 +0900

    Title: 최단 거리 9, Time: 209ms, Memory: 18MB, Status: Passed - CodeTree

--- 241115/최단 거리 9/shortest-distance-9.java
+++ 241115/최단 거리 9/shortest-distance-9.java
@@ -1,13 +1,14 @@
+
 import java.util.*;
 
 public class Main {
 
     static final int MAX_N = 1000;
 
-    static List<Node>[] graph = new List[MAX_N + 1];
+    static int[][] graph = new int[MAX_N + 1][MAX_N + 1];
     static int[] dist = new int[MAX_N + 1];
+    static boolean[] visited = new boolean[MAX_N + 1];
     static int[] path = new int[MAX_N + 1];
-    static Queue<Element> pq = new PriorityQueue<>((a, b) -> a.dist - b.dist);
 
     static int N;
     static int M;
@@ -18,17 +19,13 @@ public class Main {
         N = kb.nextInt();
         M = kb.nextInt();
 
-        for (int i = 1; i <= N; i++) {
-            graph[i] = new ArrayList<>();
-        }
-
         for (int i = 0; i < M; i++) {
             int start = kb.nextInt();
             int end = kb.nextInt();
             int dist = kb.nextInt();
 
-            graph[start].add(new Node(end, dist));
-            graph[end].add(new Node(start, dist));
+            graph[start][end] = dist;
+            graph[end][start] = dist;
         } // 세팅
 
         int S = kb.nextInt();
@@ -39,29 +36,42 @@ public class Main {
         }
         dist[S] = 0;
 
-        pq.add(new Element(S, 0));
-        while (!pq.isEmpty()) {
-            int minIndex = pq.peek().index;
-            int minDist = pq.peek().dist;
-            pq.poll();
+        // O(|V|^2) 다익스트라 코드
+        for (int i = 1; i <= N; i++) {
 
-            if (dist[minIndex] != minDist) continue;
+            // V개의 정점 중
+            // 아직 방문하지 않은 정점에서 dist 값이 가장 작은 정점을 찾아준다.
+            int minIndex = -1;
+            for (int j = 1; j <= N; j++) {
+                if (visited[j]) continue; // 이미 방문했다면 굳이 뽑을 필요가 없기 때문에
 
-            for (int j = 0; j < graph[minIndex].size(); j++) {
-                Node next = graph[minIndex].get(j);
-                int targetIndex = next.index;
-                int targetDist = next.dist;
+                if (minIndex == -1 || dist[minIndex] > dist[j]) {
+                    minIndex = j;
+                }
+            }
 
-                int newDist = minDist + targetDist;
-                if (dist[targetIndex] > newDist) {
-                    dist[targetIndex] = newDist;
-                    path[targetIndex] = minIndex;
+            // 최소값에 해당하는 정점에 방문 처리
+            visited[minIndex] = true;
 
-                    pq.add(new Element(targetIndex, newDist));
+            // 최소값에 해당하는 정점에 연결된 간선들을 보며
+            // 시작점으로부터 최단거리 값을 갱신한다.
+            for (int j = 1; j <= N; j++) {
+                // 간선이 존재하지 않으면 넘어간다.
+                if (graph[minIndex][j] == 0) continue;
+
+                if (dist[j] > dist[minIndex] + graph[minIndex][j]) {
+                    dist[j] = dist[minIndex] + graph[minIndex][j];
+                    // path 값을 갱신한다.
+                    path[j] = minIndex;
                 }
             }
         }
 
+        // 정점 S에서 정점 E로 가기 위한 최단거리를 출력한다.
+        System.out.println(dist[E]);
+
+        // 도착지 E부터 시작하여 시작점 S가 나오기 전까지
+        // path를 따라 움직인다.
         int x = E;
         List<Integer> record = new ArrayList<>();
         record.add(x);
@@ -70,9 +80,9 @@ public class Main {
             record.add(x);
         }
 
-        System.out.println(dist[E]);
+        // 거쳐간 순서를 거꾸로 출력한다.
         for (int i = record.size() - 1; i >= 0; i--) {
-            System.out.print(record.get(i) +  " ");
+            System.out.print(record.get(i) + " ");
         }
 
     }
