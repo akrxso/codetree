91cc7b6ac8e417f18ee9eccf7cd6fb95130c2a84
akrxso <143086344+akrxso@users.noreply.github.com>
2024-11-25 20:29:18 +0900

    Title: 가장 가까운 거리의 최댓값, Time: 3043ms, Memory: 95MB, Status: Time Limit Exceed - CodeTree

---
+++ 241125/가장 가까운 거리의 최댓값/maximum-of-nearest-distance.java
@@ -0,0 +1,94 @@
+import java.util.*;
+
+public class Main {
+    static final int MAX_N = 100_000;
+
+    static List<Node>[] graph = new List[MAX_N + 1];
+    static int[] dist = new int[MAX_N + 1];
+
+    static int N;
+    static int M;
+    static int a;
+    static int b;
+    static int c;
+    static int max = 0;
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner kb = new Scanner(System.in);
+        N = kb.nextInt();
+        M = kb.nextInt();
+
+        a = kb.nextInt();
+        b = kb.nextInt();
+        c = kb.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int j = 1; j <= M; j++) {
+            int start = kb.nextInt();
+            int end = kb.nextInt();
+            int dist = kb.nextInt();
+
+            graph[start].add(new Node(end, dist));
+            graph[end].add(new Node(start, dist));
+        }
+
+        
+        for (int i = 1; i <= N; i++) {
+            if (i == a || i == b || i == c) continue;
+            getPath(i);
+        }
+        System.out.println(max);
+    }
+
+    static void getPath(int startIndex) {
+        Arrays.fill(dist, 1, N, (int) 1e9);
+        Queue<Element> pq = new PriorityQueue<>((a, b) -> a.dist - b.dist);
+        dist[startIndex] = 0;
+        pq.add(new Element(startIndex, 0));
+
+        while (!pq.isEmpty()) {
+            int minIndex = pq.peek().index;
+            int minDist = pq.peek().dist;
+            pq.poll();
+
+            if (dist[minIndex] != minDist) continue;
+
+            for (int i = 0; i < graph[minIndex].size(); i++) {
+                Node target = graph[minIndex].get(i);
+                int targetIndex = target.index;
+                int targetDist = target.dist;
+
+                int newDist = minDist + targetDist;
+                if (dist[targetIndex] > newDist) {
+                    dist[targetIndex] = newDist;
+                    pq.add(new Element(targetIndex, newDist));
+                }
+            }
+        }
+
+        int min = Math.min(dist[a], dist[b]);
+        min = Math.min(min, dist[c]);
+
+        max = Math.max(max, min);
+    }
+
+    static class Node {
+        int index;
+        int dist;
+        Node(int i, int d) {
+            index = i;
+            dist = d;
+        }
+    }
+    static class Element {
+        int index;
+        int dist;
+        Element(int i, int d) {
+            index = i;
+            dist = d;
+        }
+    }
+}
