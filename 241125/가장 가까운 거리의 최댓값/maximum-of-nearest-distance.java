0129718b568497a298feb2c091c908d8779a464b
akrxso <143086344+akrxso@users.noreply.github.com>
2024-11-25 20:45:19 +0900

    Title: 가장 가까운 거리의 최댓값, Time: 1836ms, Memory: 67MB, Status: Passed - CodeTree

--- 241125/가장 가까운 거리의 최댓값/maximum-of-nearest-distance.java
+++ 241125/가장 가까운 거리의 최댓값/maximum-of-nearest-distance.java
@@ -4,6 +4,7 @@ public class Main {
     static final int MAX_N = 100_000;
 
     static List<Node>[] graph = new List[MAX_N + 1];
+
     static int[] dist = new int[MAX_N + 1];
     static int[] abcDist = new int[MAX_N + 1];
 
@@ -54,7 +55,7 @@ public class Main {
     }
 
     static void getPath(int startIndex) {
-        Arrays.fill(dist, 1, N, (int) 1e9);
+        Arrays.fill(dist, 1, N + 1, (int) 1e9);
         Queue<Element> pq = new PriorityQueue<>((a, b) -> a.dist - b.dist);
         dist[startIndex] = 0;
         pq.add(new Element(startIndex, 0));
