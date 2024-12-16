cbcd65a1d75551bcb95fc65ec0a3dc04392ccbf6
akrxso <143086344+akrxso@users.noreply.github.com>
2024-12-17 08:22:17 +0900

    Title: 정수 세 개 입력받아 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241217/정수 세 개 입력받아 출력/take-three-integers-and-output.java
+++ 241217/정수 세 개 입력받아 출력/take-three-integers-and-output.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner;
+        Scanner sc = new Scanner(System.in);
         int a = sc.newInt();
         int b = sc.newInt();
         int c = sc.newint();
