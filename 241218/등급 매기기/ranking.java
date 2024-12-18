cee63f33290960e91f8247c4ffb410883671a3d9
akrxso <143086344+akrxso@users.noreply.github.com>
2024-12-18 13:35:04 +0900

    Title: 등급 매기기, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 241218/등급 매기기/ranking.java
+++ 241218/등급 매기기/ranking.java
@@ -7,13 +7,13 @@ public class Main {
         if(w>=90)
         {
             System.out.print("A");
-        }else if(w>=90)
+        }else if(w>=80)
         {
             System.out.print("B");
-        }else if(w>=90)
+        }else if(w>=70)
         {
             System.out.print("C");
-        }else if(w>=90)
+        }else if(w>=60)
         {
             System.out.print("D");
         }else
