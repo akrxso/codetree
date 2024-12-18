9754e82b47a67162cc89c8b6f2edceb3a6429227
akrxso <143086344+akrxso@users.noreply.github.com>
2024-12-18 13:34:16 +0900

    Title: 등급 매기기, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241218/등급 매기기/ranking.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int w = sc.nextInt();
+        if(w>=90)
+        {
+            System.out.print("A");
+        }else if(w>=90)
+        {
+            System.out.print("B");
+        }else if(w>=90)
+        {
+            System.out.print("C");
+        }else if(w>=90)
+        {
+            System.out.print("D");
+        }else
+            System.out.print("F");
+    }
+}
