d94f5a6ef6cd2f0d129ff834cb665b374f44f14a
akrxso <143086344+akrxso@users.noreply.github.com>
2024-12-18 15:43:52 +0900

    Title: 정수의 조건 여부 2, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 241218/정수의 조건 여부 2/numbers-condition-2.java
+++ 241218/정수의 조건 여부 2/numbers-condition-2.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        if(a % 5 == 0 )
+        if(a == 5 )
         {
             System.out.print("A");
         }else if(a%2 == 0)
