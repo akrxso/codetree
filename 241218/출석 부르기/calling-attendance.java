31d52e8556cf4221fa1b7d2bbd9ea45d8931caad
akrxso <143086344+akrxso@users.noreply.github.com>
2024-12-18 11:38:42 +0900

    Title: 출석 부르기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241218/출석 부르기/calling-attendance.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int w = sc.nextInt();
+        if(w==1)
+        {
+        System.out.print("John");
+        }else if(w==2)
+        {
+        System.out.print("Tom");
+        }else if(w==3)
+        {
+        System.out.print("Paul");
+        }else
+        {
+        System.out.print("Vacancy");
+        }
+            }
+}
