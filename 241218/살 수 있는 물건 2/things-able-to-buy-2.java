4c6b918961397b8fc3316bf2f7158ab8dcff563d
akrxso <143086344+akrxso@users.noreply.github.com>
2024-12-18 15:11:20 +0900

    Title: 살 수 있는 물건 2, Time: 137ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241218/살 수 있는 물건 2/things-able-to-buy-2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int w = sc.nextInt();
+        if(w>=3000)
+        {
+            System.out.print("book");
+        }else if (w>=1000)
+        {
+            System.out.print("mask");
+        }else
+        {
+            System.out.print("no");
+        }
+    }
+}
