4edf9a5ad32f454d21e5d89f9b93edac91ccd00f
akrxso <143086344+akrxso@users.noreply.github.com>
2024-12-18 10:17:30 +0900

    Title: 물의 상태, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241218/물의 상태/state-of-water.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n<0)
+        {
+        System.out.print("ice");
+        }
+        else if (n>=100)
+        {
+            System.out.print("ice");
+
+        }
+        else
+        {
+            System.out.print("water");
+        }
+    }
+}
