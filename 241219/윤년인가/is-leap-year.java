2e155e7f729add0b5c7d7bec333f5b693b3bbdea
akrxso <143086344+akrxso@users.noreply.github.com>
2024-12-19 16:21:49 +0900

    Title: 윤년인가, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241219/윤년인가/is-leap-year.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+        if(y%4==0)
+        {
+            if(y%100==0 && y%400!=0)
+            {
+            System.out.print("false");
+            }else
+                System.out.print("true");
+        }
+        //100으로 나누어 떨어지되 400으로 나누어 떨어지지 않는 해
+    }
+}
