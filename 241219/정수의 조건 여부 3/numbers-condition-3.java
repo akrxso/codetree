12d1ef121220f1562816a495d539b344c5eb172c
akrxso <143086344+akrxso@users.noreply.github.com>
2024-12-19 10:09:10 +0900

    Title: 정수의 조건 여부 3, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 241219/정수의 조건 여부 3/numbers-condition-3.java
+++ 241219/정수의 조건 여부 3/numbers-condition-3.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        if(a%13==0 && a%19 ==0){
+        if(a%13==0 || a%19 ==0){
         System.out.print("True");
         }
         else{
