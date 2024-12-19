2995ee62c05c11ca7f0c29ed146639f179d764b6
akrxso <143086344+akrxso@users.noreply.github.com>
2024-12-19 09:09:54 +0900

    Title: 범위 안의 수, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 241219/범위 안의 수/number-in-range.java
+++ 241219/범위 안의 수/number-in-range.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        if(a>=10&&a>=20){
+        if(a>=10&&a<=20){
             System.out.print("yes");
         }
         else
