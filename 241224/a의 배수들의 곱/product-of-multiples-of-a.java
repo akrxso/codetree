5eb6f6cad07eb8668e81b3ebc8e94cb1fa1e2685
akrxso <143086344+akrxso@users.noreply.github.com>
2024-12-24 16:05:04 +0900

    Title: a의 배수들의 곱, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241224/a의 배수들의 곱/product-of-multiples-of-a.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int mult = 1 ;
+        for(int i=a;i<=b;i++ )
+        {
+            if(i%a==0)
+            {
+                mult*=i;
+            }
+        }
+        System.out.print(mult);
+    }
+}
