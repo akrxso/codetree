ac5a240496d866e56c8f5c5bbc9b4ca1c74f61f6
akrxso <143086344+akrxso@users.noreply.github.com>
2024-12-24 16:35:04 +0900

    Title: 조건에 부합하는 수, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 241224/조건에 부합하는 수/number-that-meets-the-condition.java
+++ 241224/조건에 부합하는 수/number-that-meets-the-condition.java
@@ -6,7 +6,7 @@ public class Main {
         int a = sc.nextInt();
         for(int i=1;i<=a;i++)
         {
-            if(i%2==0&&a%4!=0)
+            if(i%2==0&&i%4!=0)
             {
                 continue;
             }else if((i/8)%2==0)
