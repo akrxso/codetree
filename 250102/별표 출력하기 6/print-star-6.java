c340e61204fff5e811ccb63c3067f71999e5019c
akrxso <143086344+akrxso@users.noreply.github.com>
2025-01-02 13:43:27 +0900

    Title: 별표 출력하기 6, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250102/별표 출력하기 6/print-star-6.java
+++ 250102/별표 출력하기 6/print-star-6.java
@@ -20,9 +20,9 @@ public class Main {
         }
 
         //3 5 7
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n-1;i++)
         {
-            for(int j=0;j<n-i;j++)
+            for(int j=0;j<n-2-i;j++)
             {
             System.out.print("  ");
             }
