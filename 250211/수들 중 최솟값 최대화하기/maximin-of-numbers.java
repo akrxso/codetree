3f5f7828c3cb6de950b37d4709cf74e2695371b8
akrxso <143086344+akrxso@users.noreply.github.com>
2025-02-11 15:09:48 +0900

    Title: 수들 중 최솟값 최대화하기, Time: 520ms, Memory: 25MB, Status: Passed - CodeTree

--- 250211/수들 중 최솟값 최대화하기/maximin-of-numbers.java
+++ 250211/수들 중 최솟값 최대화하기/maximin-of-numbers.java
@@ -6,44 +6,57 @@ public class Main {
     
     static int N;
     static int[][] grid = new int[MAX_N][MAX_N];
-    static boolean[] visited = new boolean[MAX_N];
-    static List<Integer> colIdxs = new ArrayList<>();
+    static List<Pair> pairs = new ArrayList<>();
 
     static int max = Integer.MIN_VALUE;
-
+    
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
         N = kb.nextInt();
-
+        
         for (int r = 0; r < N; r++)
             for (int c = 0; c < N; c++)
                 grid[r][c] = kb.nextInt();
-
+    
         getMin(0);
         System.out.println(max);
     }
-
+    
     static void getMin(int row) {
         // 기저 조건
         if (row == N) {
-            int min = grid[0][colIdxs.get(0)];
-            for (int i = 1; i < colIdxs.size(); i++) {
-                min = Math.min(min, grid[i][colIdxs.get(i)]);
+            int min = Integer.MAX_VALUE;
+            for (Pair pair : pairs) {
+                min = Math.min(min, grid[pair.row][pair.col]);
             }
             max = Math.max(max, min);
+            return;
         }
-
+        
         for (int c = 0; c < N; c++) {
-            if (visited[c]) continue;
-
-            visited[c] = true;
-            colIdxs.add(c);
-            
-            getMin(row + 1);
-
-            visited[c] = false;
-            colIdxs.remove(colIdxs.size() - 1);
-
+            if (isAvailable(row, c)) {
+                pairs.add(new Pair(row, c));
+                getMin(row + 1);
+                pairs.remove(pairs.size() - 1);
+            }
+        }
+    }
+    
+    static boolean isAvailable(int r, int c) {
+        for (Pair pair : pairs) {
+            if (pair.row == r || pair.col == c) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    static class Pair {
+        int row;
+        int col;
+        Pair(int r, int c) {
+            row = r;
+            col = c;
         }
     }
 }
