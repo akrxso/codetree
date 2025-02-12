32142c1f97882cfc8815c56034fb58ee69567cde
akrxso <143086344+akrxso@users.noreply.github.com>
2025-02-12 14:06:04 +0900

    Title: 그래프 탐색, Time: 455ms, Memory: 28MB, Status: Passed - CodeTree

--- 250212/그래프 탐색/graph-traversal.java
+++ 250212/그래프 탐색/graph-traversal.java
@@ -1,43 +1,39 @@
 import java.util.*;
 
 public class Main {
+    
+    static final int MAX_N = 1000;
 
     static int N;
     static int M;
-    static int[][] adjMatrix; 
-    static boolean[] visited;
+    static int cnt;
 
-    static int max = Integer.MIN_VALUE;
-
+    static int[][] graph = new int[MAX_N + 1][MAX_N + 1];
+    static boolean[] visited = new boolean[MAX_N + 1];
+    
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
         N = kb.nextInt();
         M = kb.nextInt();
-
-        adjMatrix = new int[N + 1][N + 1];
-        visited = new boolean[N + 1];
-
+        
         for (int i = 0; i < M; i++) {
             int start = kb.nextInt();
             int end = kb.nextInt();
-            adjMatrix[start][end] = adjMatrix[end][start] = 1;
+            graph[start][end] = graph[end][start] = 1;
         }
+        
         visited[1] = true;
         dfs(1);
-        
-        int cnt = 0;
-        for (int i = 2; i < visited.length; i++) {
-            if (visited[i]) cnt++;
-        }
 
         System.out.println(cnt);
     }
-
+    
     static void dfs(int vertex) {
         for (int next = 1; next <= N; next++) {
-            if (adjMatrix[vertex][next] == 1 & !visited[next]) {
+            if (graph[vertex][next] == 1 && !visited[next]) {
                 visited[next] = true;
                 dfs(next);
+                cnt++;
             }
         }
     }
