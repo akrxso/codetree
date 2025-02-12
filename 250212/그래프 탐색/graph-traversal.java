37ba3c7113a9ff1a6f069c9e3ba3992d81f5d732
akrxso <143086344+akrxso@users.noreply.github.com>
2025-02-12 13:27:30 +0900

    Title: 그래프 탐색, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250212/그래프 탐색/graph-traversal.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+
+    static int N;
+    static int M;
+    static int[][] adjMatrix; 
+    static boolean[] visited;
+
+    static int max = Integer.MIN_VALUE;
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        N = kb.nextInt();
+        M = kb.nextInt();
+
+        adjMatrix = new int[N + 1][N + 1];
+        visited = new boolean[N + 1];
+
+        for (int i = 0; i < M; i++) {
+            int start = kb.nextInt();
+            int end = kb.nextInt();
+            adjMatrix[start][end] = adjMatrix[end][start] = 1;
+        }
+        visited[1] = true;
+        dfs(1, 0);
+        System.out.println(max);   
+    }
+
+    static void dfs(int vertex, int cnt) {
+        max = Math.max(max, cnt);
+
+        for (int next = 1; next <= N; next++) {
+            if (adjMatrix[vertex][next] == 1 & !visited[next]) {
+                visited[next] = true;
+                dfs(next, cnt + 1);
+            }
+        }
+    }
+}
