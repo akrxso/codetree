7b05e0d4a3d133191f3fd53e39d437e2a636e9c0
akrxso <143086344+akrxso@users.noreply.github.com>
2025-02-26 20:38:35 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 137ms, Memory: 11MB, Status: Passed - CodeTree

--- 250226/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.java
+++ 250226/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.java
@@ -1,78 +1,90 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
+
     static final int MAX_N = 100;
-    static int[][] maze = new int[MAX_N][MAX_N];
-    static boolean[][] visited = new boolean[MAX_N][MAX_N];
-    
+    static final int DIR_NUM = 4;
+
     static int n;
-    static int startRow;
-    static int startCol;
-    static int cnt = 0;
 
-    static int[] dr = {0, -1, 0, 1};
+    static int currRow;
+    static int currCol;
+    static int currDir;
+
+    static int[][] grid = new int[MAX_N][MAX_N];
+    static boolean[][][] visited = new boolean[DIR_NUM][MAX_N][MAX_N];
+    static int[] dr = {0, 1, 0, -1};
     static int[] dc = {1, 0, -1, 0};
-
-    static int curDir = 0;
-
+    static int time;
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        startRow = sc.nextInt() - 1;
-        startCol = sc.nextInt() - 1;
+        Scanner kb = new Scanner(System.in);
+        n = kb.nextInt();
+        currRow = kb.nextInt() - 1;
+        currCol = kb.nextInt() - 1;
+        // 처음에 우측 방향
+        currDir = 0;
 
-        for (int i = 0; i < n; i++) {
-            String line = sc.next();
-            for (int j = 0; j < n; j++) {
-                maze[i][j] = line.charAt(j) == '#' ? 1 : 0;
+        for (int r = 0; r < n; r++) {
+            String str = kb.next();
+            for (int c = 0; c < n; c++) {
+                grid[r][c] = str.charAt(c) == '#' ? 1 : 0;
             }
         }
-        
-        int curRow = startRow;
-        int curCol = startCol;
-        visited[curRow][curCol] = true;
 
-        while (true) {
-            int nextRow = curRow + dr[curDir];
-            int nextCol = curCol + dc[curDir];
+        do {
+            simulate();
+        } while (inRange(currRow, currCol));
 
-            // 경계선 밖이면 끝
-            if (nextRow < 0 || n <= nextRow || nextCol < 0 || n <= nextCol) {
-                cnt++;
-                break;
+        System.out.println(time);
+    }
+
+    static void simulate() {
+        if (visited[currDir][currRow][currCol]) {
+            System.out.println(-1);
+            System.exit(0);
+        }
+
+        // 현재 상황이 다시 반복되는지를 나중에 확인하기 위해
+        // 현재 상황에 해당하는 곳에 visited 값을 true로 설정한다.
+        visited[currDir][currRow][currCol] = true;
+
+        int nextRow = currRow + dr[currDir];
+        int nextCol = currCol + dc[currDir];
+
+        // 바라보는 방향이 벽이라면
+        if (inRange(nextRow, nextCol) && grid[nextRow][nextCol] == 1) {
+            currDir = (currDir - 1 + 4) % 4;
+        }
+        // 바라보는 방향이 벽이 아니라면
+        // 만약 바로 앞이 격자 밖이라면 탈출
+        else if (!inRange(nextRow, nextCol)) {
+            currRow = nextRow;
+            currCol = nextCol;
+            time++;
+        }
+        // 바로 앞이 격자 안이라면
+        else {
+            int blockRow = nextRow + dr[(currDir + 1) % 4];
+            int blockCol = nextCol + dc[(currDir + 1) % 4];
+
+            // 오른쪽에 벽이 있다면
+            if (inRange(blockRow, blockCol) && grid[blockRow][blockCol] == 1) {
+                currRow = nextRow;
+                currCol = nextCol;
+                time++;
             }
-            if (maze[nextRow][nextCol] == 1) { // 장애물이면
-                curDir = (curDir + 1) % 4;
-            }
-            else { // 장애물이 아니면
-                int blockDir = curDir - 1 < 0 ? 3 : curDir - 1;
-                int blockRow = nextRow + dr[blockDir];
-                int blockCol = nextCol + dc[blockDir];
-                // 경계 처리 해야 하나?
-                if (maze[blockRow][blockCol] == 1) { // 벽이 있으면 한 칸 그대로 이동
-                    if (visited[nextRow][nextCol]) {
-                        cnt = -1;
-                        break;
-                    }
-                    curRow = nextRow;
-                    curCol = nextCol;
-                    visited[curRow][curCol] = true;
-                    cnt++;
-                }
-                else { // 벽이 없다면
-                    if (visited[blockRow][blockCol]) {
-                        cnt = -1;
-                        break;
-                    }
-                    curRow = blockRow;
-                    curCol = blockCol;
-                    curDir = blockDir;
-                    visited[curRow][curCol] = true;
-                    cnt += 2;
-                }
+            // 벽이 없다면
+            else {
+                currRow = blockRow;
+                currCol = blockCol;
+                currDir = (currDir + 1) % 4;
+                time += 2;
             }
         }
-        System.out.println(cnt);
+    }
+
+    static boolean inRange(int r, int c) {
+        return 0 <= r && r < n && 0 <= c && c < n;
     }
 }
