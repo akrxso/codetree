ff851cb4c2749effa9b9c071ff9d2ae9829eb52e
akrxso <143086344+akrxso@users.noreply.github.com>
2025-02-27 11:28:19 +0900

    Title: 대폭발, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250227/대폭발/big-explosion.java
+++ 250227/대폭발/big-explosion.java
@@ -1,16 +1,56 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    static final int MAX_N = 100;
-    static int[][] grid = new int[MAX_N][MAX_N];
 
-    static int n;
-    static int m;
-    static int row;
-    static int col;
-    static Set<String> bombs = new HashSet<>();
-    static int[] dr = {-1, 1, 0, 0};
-    static int[] dc = {0, 0, -1, 1};
+    static final int MAX_N = 100;
+    static final int DIR_NUM = 4;
+
+    static int n, m, row, col;
+    static int[][] grid = new int[MAX_N][MAX_N];
+    static int[][] nextGrid = new int[MAX_N][MAX_N];
+
+    static boolean inRange(int row, int col) {
+        return 0 <= row && row < n && 0 <= col && col < n;
+    }
+
+    static void expand(int r, int c, int dist) {
+        int[] dr = {-1, 1, 0, 0};
+        int[] dc = {0, 0, -1, 1};
+        for (int d = 0; d < DIR_NUM; d++) {
+            int nextRow = r + dr[d] * dist;
+            int nextCol = c + dc[d] * dist;
+            if (inRange(nextRow, nextCol)) {
+                nextGrid[nextRow][nextCol] = 1;
+            }
+        }
+    }
+
+    static void simulate(int dist) {
+        // Step1. nextGrid의 값을 0으로 초기화한다.
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < n; c++) {
+                nextGrid[r][c] = 0;
+            }
+        }
+
+        // Step2. 폭탄을 던지는 시뮬레이션을 진행한다.
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < n; c++) {
+                if (grid[r][c] > 0) {
+                    expand(r, c, dist);
+                }
+            }
+        }
+
+        // Step 3. nextGrid 값을 grid로 업데이트 한다.
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < n; c++) {
+                if (nextGrid[r][c] > 0) {
+                    grid[r][c] = 1;
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
@@ -19,50 +59,18 @@ public class Main {
         row = kb.nextInt() - 1;
         col = kb.nextInt() - 1;
 
-        bombs.add(convert(row, col));
-
-        for (int t = 1; t <= m; t++) {
-            int value = 1;
-            int cnt = t - 1;
-            for (int i = 0; i < cnt; i++) {
-                value *= 2;
-            }
-
-            Set<String> nextBombs = new HashSet<>(bombs);
-            // System.out.println("[bombs] :" + bombs);
-            // System.out.println(t + "] before : " + nextBombs);
-            // System.out.println("value : " + value);
-
-            for (String bomb : bombs) {
-                String[] s = bomb.split(" ");
-                int curRow = Integer.valueOf(s[0]);
-                int curCol = Integer.valueOf(s[1]);
-
-                // System.out.println(bomb + " -> " + curRow + " & " + curCol);
-
-                for (int d = 0; d < 4; d++) {
-                    int nextRow = curRow + (dr[d] * value);
-                    int nextCol = curCol + (dc[d] * value);
-
-                    // 범위 안이면
-                    if (inRange(nextRow, nextCol)) {
-                        nextBombs.add(convert(nextRow, nextCol));
-                    }
-                }
-            }
-            // System.out.println(t + "] after : " + nextBombs);
-            bombs = nextBombs;
+        int dist = 1;
+        for (int i = 0; i < m; i++) {
+            simulate(dist);
+            dist *= 2;
         }
-
-        // System.out.println(bombs);
-        System.out.println(bombs.size());
-    }
-
-    static boolean inRange(int row, int col) {
-        return 0 <= row && row < n && 0 <= col && col < n;
-    }
-
-    static String convert(int row, int col) {
-        return row + " " + col;
+        
+        int ans = 0;
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < n; c++) {
+                ans += grid[r][c];
+            }
+        }
+        System.out.println(ans);
     }
 }
