aa2187a9365b5e4eece6a168a6022938896ba7cd
akrxso <143086344+akrxso@users.noreply.github.com>
2025-02-28 17:03:26 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 1378ms, Memory: 80MB, Status: Passed - CodeTree

--- 250228/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
+++ 250228/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
@@ -1,85 +1,86 @@
 import java.util.*;
 
 public class Main {
+
+    static final int DIR_NUM = 4;
     static final int MAX_N = 500;
+
+    // 변수 선언
+    static int N;
     static int[][] grid = new int[MAX_N][MAX_N];
     static int[][] dp = new int[MAX_N][MAX_N];
 
-    static int N;
-    static int[] dr = {-1, 1, 0, 0};
-    static int[] dc = {0, 0, -1, 1};
+    static List<Cell> cells = new ArrayList<>();
     static int ans = 0;
 
-    static boolean inRange(int row, int col) {
-        return 0 <= row && row < N && 0 <= col && col < N;
+    static boolean inRange(int r, int c) {
+        return 0 <= r && r < N && 0 <= c && c < N;
     }
-    static void simulate(int row, int col) {
-        int max = 0;
-        for (int d = 0; d < 4; d++) {
-            int nextRow = row + dr[d];
-            int nextCol = col + dc[d];
-
-            // 경계 안에 있고, 해당 값이 grid보다 작을 때
-            if (inRange(nextRow, nextCol) && grid[nextRow][nextCol] < grid[row][col]) {
-                max = Math.max(max, dp[nextRow][nextCol]);
-            }
-        }
-        dp[row][col] = max + 1;
-        ans = Math.max(ans, dp[row][col]);
-    }
-
     public static void main(String[] args) {
-        Scanner kb = new Scanner(System.in);
-        N = kb.nextInt();
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
 
         for (int r = 0; r < N; r++) {
             for (int c = 0; c < N; c++) {
-                grid[r][c] = kb.nextInt();
+                grid[r][c] = sc.nextInt();
             }
         }
 
-        Set<Integer> set = new HashSet<>();
+        // 각 칸에 적혀있는 정수값 기준으로
+        // 오름차순으로 되도록 칸의 위치들을 정렬한다
+        // 편하게 정렬하기 위해 (칸에 적혀 있는 값, 행 위치, 열 위치) 순으로 넣어준다.
         for (int r = 0; r < N; r++) {
             for (int c = 0; c < N; c++) {
-                set.add(grid[r][c]);
+                cells.add(new Cell(grid[r][c], r, c));
             }
         }
-        List<Integer> numbers = new ArrayList<>(set);
-        Collections.sort(numbers);
 
-        // 초기 세팅
-        int minNumber = numbers.get(0);
+        Collections.sort(cells, new Comparator<Cell>() {
+            @Override
+            public int compare(Cell o1, Cell o2) {
+                return o1.num - o2.num;
+            }
+        });
+
+        // 처음 DP 값들은 전부 1로 초기화한다.
+        // (해당 칸에서 시작하는 경우이므로)
         for (int r = 0; r < N; r++) {
             for (int c = 0; c < N; c++) {
-                if (minNumber == grid[r][c]) { // 가장 낮은수랑 같다면
-                    dp[r][c] = 1;
+                dp[r][c] = 1;
+            }
+        }
+
+        // 정수값이 작은칸부터 순서대로 보며,
+        // 4방향에 대한 dp 값을 갱신해준다.
+        for (int i = 0; i < cells.size(); i++) {
+            int r = cells.get(i).r;
+            int c = cells.get(i).c;
+            int[] dr = {-1, 1, 0, 0};
+            int[] dc = {0, 0, -1, 1};
+
+            // 인접한 4칸에 대해 갱신을 진횅한다.
+            for (int d = 0; d < DIR_NUM; d++) {
+                int nextRow = r + dr[d];
+                int nextCol = c + dc[d];
+                if (inRange(nextRow, nextCol) && grid[nextRow][nextCol] > grid[r][c]) {
+                    dp[nextRow][nextCol] = Math.max(dp[nextRow][nextCol], dp[r][c] + 1);
                 }
             }
         }
-
-        for (int i = 1; i < numbers.size(); i++) { // 그 다음 숫자부터
-            int number = numbers.get(i);
-//            System.out.println("number : " + number);
-            for (int r = 0; r < N; r++) {
-                for (int c = 0; c < N; c++) {
-                    if (grid[r][c] == number) {
-                        simulate(r, c);
-                    }
-                }
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+                ans = Math.max(ans, dp[r][c]);
             }
-//            print();
         }
         System.out.println(ans);
-
     }
 
-    static void print() {
-        for (int r = 0; r < N; r++) {
-            for (int c = 0; c < N; c++) {
-                System.out.print(dp[r][c] + " ");
-            }
-            System.out.println();
+    static class Cell {
+        int num, r, c;
+        public Cell(int num, int r, int c) {
+            this.num = num;
+            this.r = r;
+            this.c = c;
         }
-        System.out.println();
     }
 }
